0cc18564560523a3acb90a23d6f8f1aa566bf303
PeterChen123 <chenguanyu9745@outlook.com>
2019-11-06 12:40:24 -0800

    First commit of project files

---
+++ JavaFX_Project/src/com/guanyuchen/todolist/Controller.java
@@ -0,0 +1,104 @@
+package com.guanyuchen.todolist;
+
+import com.guanyuchen.todolist.datamodel.TodoData;
+import com.guanyuchen.todolist.datamodel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+public class Controller {
+
+    private List<TodoItem> todoItems;
+    @FXML
+    private ListView<TodoItem> todoListView;
+    @FXML
+    private TextArea itemDetailsTextArea;
+    @FXML
+    private Label deadlineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
+
+    public void initialize() {
+//        TodoItem item1 = new TodoItem("Mail birthday card", "Buy something to yourself",
+//                LocalDate.of(2019, Month.APRIL, 25));
+//        TodoItem item2 = new TodoItem("Doctor's Appointment", "Meet at the doctor's place",
+//                LocalDate.of(2019, Month.APRIL, 25));
+//        TodoItem item3 = new TodoItem("Mail birthday card", "Buy something to yourself",
+//                LocalDate.of(2019, Month.APRIL, 25));
+//        TodoItem item4 = new TodoItem("Mail birthday card", "Buy something to yourself",
+//                LocalDate.of(2019, Month.APRIL, 25));
+//        TodoItem item5 = new TodoItem("Mail birthday card", "Buy something to yourself",
+//                LocalDate.of(2019, Month.APRIL, 25));
+//
+//        todoItems = new ArrayList<TodoItem>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//
+//        TodoData.getInstance().setTodoItems(todoItems);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observableValue, TodoItem todoItem, TodoItem t1) {
+                if(t1 != null) {
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
+                }
+            }
+        });
+
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void showNewItemDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        dialog.setTitle("Add New Todo Item");
+        dialog.setHeaderText("Use this dialog to create a new todo item");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("todoItemDialog.fxml"));
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+            DialogController controller = fxmlLoader.getController();
+            TodoItem newItem = controller.processResults();
+            todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+            todoListView.getSelectionModel().select(newItem);
+            System.out.println("OK clicked.");
+        } else {
+            System.out.println("Cancel clicked.");
+        }
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
+    }
+}
---
+++ JavaFX_Project/src/module-info.java
@@ -0,0 +1,6 @@
+module JavaFX.Project {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens com.guanyuchen.todolist;
+}
